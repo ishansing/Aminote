6d6c20fde91433099fd6ee2295c7b13003270a19
ishansing <ishan.nva.2005@gmail.com>
2025-07-17 01:39:46 +0530

    Auth setup done & Database connected

---
+++ convex/auth.config.js
@@ -0,0 +1,11 @@
+//rapid-teal-24.clerk.accounts.dev
+
+export default {
+  providers: [
+    {
+      domain: "https://rapid-teal-24.clerk.accounts.dev",
+
+      applicationID: "convex",
+    },
+  ],
+};
