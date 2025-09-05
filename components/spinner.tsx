afccc857e3b0f2fd09afcea04b03d87977490190
ishansing <ishan.nva.2005@gmail.com>
2025-09-05 23:24:32 +0530

    interface error fix

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -16,7 +16,8 @@ const spinnerVariants = cva("text-muted-foreground animate-spin", {
   },
 });
 
-interface SpinnerProps extends VariantProps<typeof spinnerVariants> {}
+// Fix: Use type alias instead of empty interface
+type SpinnerProps = VariantProps<typeof spinnerVariants>;
 
 export const Spinner = ({ size }: SpinnerProps) => {
   return <Loader className={cn(spinnerVariants({ size }))} />;
